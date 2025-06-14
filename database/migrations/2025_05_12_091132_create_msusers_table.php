c3dd3a6b92442b602d30f79e0bfe3d05a1f843ce
medlynhan <medlynhan@gmail.com>
2025-06-15 00:31:04 +0700

    seed users

--- database/migrations/2025_05_12_091132_create_msusers_table.php
+++ database/migrations/2025_05_12_091132_create_msusers_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('msusers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
